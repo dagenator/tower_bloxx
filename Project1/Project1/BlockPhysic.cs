9c5e18fb57639841f5c9b7a8887cfc80f41c98c5
Darina <gabidullinadarina.01@gmail.com>
2020-04-03 11:57:17 +0500

    DirectX for manged code add

---
+++ Project1/Project1/BlockPhysic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class BlockPhysic
+    {
+        public const float gravity = 9.8f;
+
+        private bool isFalling = false;
+        private 
+    }
+}
