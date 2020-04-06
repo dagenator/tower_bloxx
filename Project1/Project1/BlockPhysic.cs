a0c2d02f6f557b45d0759eb2d11bf42b9b52f118
Darina <gabidullinadarina.01@gmail.com>
2020-04-06 20:15:20 +0500

    WPF window add

--- Project1/Project1/BlockPhysic.cs
+++ Project1/Project1/BlockPhysic.cs
@@ -6,11 +6,39 @@ using System.Threading.Tasks;
 
 namespace Project1
 {
+    public class Vector
+    {
+        readonly int X;
+        readonly int Y;
+        public Vector(int inputX, int inputY)
+        {
+            X = inputX;
+            Y = inputY;
+        }
+    }
+
     class BlockPhysic
     {
-        public const float gravity = 9.8f;
+        // public const float gravity = 9.8f;
+        public Vector Position { get; private set; }
+        public Vector Size { get; private set; }
+        public bool isFalling  { get; private set; }
+        public BlockPhysic(int posX, int posY, int sizeX, int sizeY)
+        {
+            Position = new Vector(posX, posY);
+            Size = new Vector(sizeX, sizeY);
+        }
 
-        private bool isFalling = false;
-        private 
+        public void ChangeBlockStatement(BlockPhysic block)
+        {
+            isFalling = !isFalling;
+        }
+       
+        public event Action<int, int> BlockFalling;
+        
+        //private void FallingMove(this BlockPhysic block, PaintEventArgs)
+        //{
+
+        //}
     }
 }
