dd5f2e1228d1bca6eba136b8b76f9264bc6d8c72
Darina <gabidullinadarina.01@gmail.com>
2020-04-06 20:19:04 +0500

    Some minimum black square add

---
+++ WpfApp1/WpfApp1/BlockPhysic.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Vector
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+        public Vector(double inputX, double inputY)
+        {
+            X = inputX;
+            Y = inputY;
+        }
+    }
+
+    public class BlockPhysic
+    {
+        // public const float gravity = 9.8f;
+        public Vector Position { get; private set; }
+        public Vector Size { get; private set; }
+        public bool isFalling { get; private set; }
+
+        public event Action<Vector> BlockFalling;
+        public BlockPhysic(int posX, int posY, int sizeX, int sizeY)
+        {
+            Position = new Vector(posX, posY);
+            Size = new Vector(sizeX, sizeY);
+        }
+
+        public void ChangeBlockStatement()
+        {
+            this.isFalling = !this.isFalling;
+            MovingDown();
+
+            //if (BlockFalling != null) BlockFalling(Size);
+        }
+
+        private void MovingDown()
+        {
+            Position = new Vector(Position.X, Position.Y + 20);
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    
+
+    public partial class MainWindow : Window
+    {
+        private BlockPhysic block = new BlockPhysic(200, 50, 70, 70);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var rectangle = MakeRectangle(block.Position, block.Size, Brushes.Black);
+            
+            CanvasField.Children.Insert(0, rectangle);
+
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                block.ChangeBlockStatement();
+
+            }
+        }
+
+        private Rectangle MakeRectangle(Vector position, Vector size, Brush brush)
+        {
+            
+            var rect = new Rectangle();
+            rect.Width = size.X;
+            rect.Height = size.Y;
+            rect.Fill = brush;
+            Canvas.SetLeft(rect, position.X);
+            Canvas.SetTop(rect, position.Y);
+            return rect;
+        }
+    }
+}
